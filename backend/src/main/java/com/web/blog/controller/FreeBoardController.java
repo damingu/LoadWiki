4ea10f4ec4a5a8c0a9610a8b802fed53ea9cdda7
Keonsang Yun <kesay59@gmail.com>
2021-01-25 14:11:09 +0900

    [feat] board controller

--- backend/src/main/java/com/web/blog/controller/FreeBoardController.java
+++ backend/src/main/java/com/web/blog/controller/FreeBoardController.java
@@ -20,14 +20,6 @@ import com.web.blog.model.service.FreeBoardService;
 @RequestMapping("/freeboard")
 public class FreeBoardController {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
-//	public Object getPostingListAll(String page, String...tags);
-//	public Object getPostingListByName(String page, String name, String...tags);
-//	public Object getPostingListByTitle(String page, String title, String...tags);
-//	public Object getPostingListByContent(String page, String content, String...tags);
-//	public Object getPosting(String pid);
-//	public Object regist(Posting posting);
-//	public Object edit(Posting posting);
-//	public Object delete(String pid);
 	
 	@Autowired
 	FreeBoardService fBoardServ;
