3e13f30dcada615ea4f32d03bc83dd33e43b9559
whaudg98 <whaudg98@naver.com>
2021-01-25 14:12:05 +0900

    [feat] 회원가입 폼 관심 개발 분야 수정

--- frontend/src/views/Pages/Register/PartCheckboxes.vue
+++ frontend/src/views/Pages/Register/PartCheckboxes.vue
@@ -2,16 +2,29 @@
   <div>
     <b-form-group label="관심 개발 분야" v-slot="{ ariaDescribedby }">
       <p>1순위부터 체크해주세요</p>
-      <b-form-checkbox-group
-        id="checkbox-group-1"
-        v-model="selected"
-        :options="options"
-        :aria-describedby="ariaDescribedby"
-        name="flavour-1"
-      >
-      </b-form-checkbox-group>
+      <b-container>
+        <b-form-checkbox
+          v-for="option in options"
+          v-model="selected"
+          :key="option.value"
+          :value="option.value"
+          :aria-describedby="ariaDescribedby"
+          name="flavour-3a"
+        >
+          {{ option.text }}
+        </b-form-checkbox>
+        <!-- <b-form-checkbox-group
+          id="checkbox-group-1"
+          v-model="selected"
+          :options="options"
+          :aria-describedby="ariaDescribedby"
+          name="flavour-1"
+        >
+        </b-form-checkbox-group> -->
+
+      </b-container>
     </b-form-group>
-    
+
     <div>1순위 <strong>{{ selected[0] }}</strong></div>
     <div>2순위 <strong>{{ selected[1] }}</strong></div>
     <div>3순위 <strong>{{ selected[2] }}</strong></div>
