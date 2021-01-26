53ba23844fbd764bb7550bc81b925860aafe2189
whaudg98 <whaudg98@naver.com>
2021-01-27 04:48:53 +0900

    [feat] 회원정보 수정 페이지 추가

---
+++ frontend/src/components/Profileupdate/FlavourContent.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <b-button @click="modalShow = !modalShow">관심 개발 분야 수정</b-button>
+
+    <b-modal v-model="modalShow" hide-footer>
+      <template #modal-title>
+        <h1>관심 개발 분야</h1>
+      </template>
+      <div>
+        <b-form-group label="" v-slot="{ ariaDescribedby }">
+          <p>1순위부터 체크해주세요</p>
+          <b-container>
+            <b-form-checkbox
+              v-for="option in options"
+              v-model="selected"
+              :key="option.value"
+              :value="option.value"
+              :aria-describedby="ariaDescribedby"
+              name="flavour-3a"
+            >
+              {{ option.text }}
+            </b-form-checkbox>
+          </b-container>
+        </b-form-group>
+        <b-row class="justify-content-md-center">
+          <b-col class="ml-3">1순위</b-col> 
+          <b-col v-if="selected.length > 2">{{ options[selected[0]-1].text }}</b-col>
+        </b-row>
+        <b-row class="justify-content-md-center">
+          <b-col class="ml-3">2순위</b-col> 
+          <b-col v-if="selected.length > 2">{{ options[selected[0]-1].text }}</b-col>
+        </b-row>
+        <b-row class="justify-content-md-center">
+          <b-col class="ml-3">3순위</b-col> 
+          <b-col v-if="selected.length > 2">{{ options[selected[0]-1].text }}</b-col>
+        </b-row>
+      </div>
+      <div class="text-center">
+        <base-button type="primary" native-type="submit" class="my-4" @click="sendFlavour">확인</base-button>
+      </div>
+    </b-modal>
+  </div>
+  <!-- <b-modal id="modal-login">
+  </b-modal> -->
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      modalShow: false,
+        selected: [],
+        options: [
+          { text: 'Python', value: 1},
+          { text: 'JAVA', value: 2},
+          { text: 'C', value: 3},
+          { text: 'Vue', value: 4},
+          { text: 'Spring', value: 5},
+          { text: 'Frontend', value: 6},
+          { text: 'Backend', value: 7},
+          { text: 'Database', value: 8},
+          { text: 'AI', value: 9},
+          { text: '기타', value: 10},
+        ],
+      }
+    },
+  methods: {
+    sendFlavour() {
+      this.modalShow = false
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/Profileupdate/ProfileImg.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <b-button size="sm" @click="modalShow = !modalShow">사진📷</b-button>
+
+    <b-modal v-model="modalShow" hide-footer>
+      <template #modal-title>
+        <h1>프로필 업로드</h1>
+      </template>
+      <div>
+        <b-form-file
+          v-model="file1"
+          :state="Boolean(file1)"
+          placeholder="Choose a file or drop it here..."
+          drop-placeholder="Drop file here..."
+        ></b-form-file>
+        <div class="mt-3">Selected file: {{ file1 ? file1.name : '' }}</div>
+      </div>
+      <div class="text-center">
+        <base-button type="primary" native-type="submit" class="my-4" @click="modalShow = false">확인</base-button>
+      </div>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      modalShow: false,
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
